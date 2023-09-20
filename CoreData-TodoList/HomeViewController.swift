006ee836a291c176cf4834d8ee234ca002a10ad0
se-ryeong <seryz029@gmail.com>
2023-09-21 02:38:26 +0900

    [FEAT]:profile버튼 추가

--- CoreData-TodoList/HomeViewController.swift
+++ CoreData-TodoList/HomeViewController.swift
@@ -15,8 +15,16 @@ class HomeViewController: UIViewController {
         todoListButton.translatesAutoresizingMaskIntoConstraints = false
         todoListButton.setTitleColor(.darkGray, for: .normal)
         
-        
         return todoListButton
+    }()
+    
+    let profileButton : UIButton = {
+        let profileButton = UIButton()
+        profileButton.setTitle("Insta-Profile", for: .normal)
+        profileButton.translatesAutoresizingMaskIntoConstraints = false
+        profileButton.setTitleColor(.darkGray, for: .normal)
+        
+        return profileButton
     }()
     
     override func viewDidLoad() {
@@ -27,19 +35,30 @@ class HomeViewController: UIViewController {
     
     private func setupUI() {
         view.addSubview(todoListButton)
+        view.addSubview(profileButton)
         
         NSLayoutConstraint.activate([
             todoListButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            todoListButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            todoListButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50),
+            
+            profileButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            profileButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         
         ])
         
-        todoListButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        todoListButton.addTarget(self, action: #selector(todoListbuttonTapped), for: .touchUpInside)
+        profileButton.addTarget(self, action: #selector(profilebuttonTapped), for: .touchUpInside)
     }
     
-    @objc func buttonTapped() {
+    @objc func todoListbuttonTapped() {
         let listVC = ListVC()
         navigationController?.pushViewController(listVC, animated: true)
     }
     
+    @objc func profilebuttonTapped() {
+        let profileVC = ProfileDesignVC()
+        present(profileVC, animated: true, completion: nil)
+        
+        
+    }
 }
--- CoreData-TodoList/ProfileDesignVC.swift
+++ CoreData-TodoList/ProfileDesignVC.swift
@@ -8,5 +8,29 @@
 import UIKit
 
 class ProfileDesignVC: UIViewController {
+    let username: UILabel = {
+        let username = UILabel()
+        username.text = "nabaecamp"
+        username.font = UIFont.boldSystemFont(ofSize: 18)
+        username.textColor = .black
+        username.translatesAutoresizingMaskIntoConstraints = false
+        username.textAlignment = .center
+        
+        return username
+    }()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+    }
+    
+    private func setUI() {
+        view.addSubview(username)
+        
+        NSLayoutConstraint.activate([
+            username.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+            username.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        
+        ])
+    }
 }
